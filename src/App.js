38175763f04efa008a766bfb3e630376c7f20818
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-16 17:02:08 -0300

    feat: Get Metamask Provider with link account

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { getMetaMaskProvider } from './MetaMaskService';
+
 function App() {
-  return <div>Olá Mundo</div>;
+  return (
+    <div>
+      Olá Mundo
+      <br />
+      <button onClick={() => getMetaMaskProvider()}>Click Me</button>
+    </div>
+  );
 }
 
 export default App;
--- src/MetaMaskService.js
+++ src/MetaMaskService.js
@@ -1,3 +1,13 @@
 import Web3 from 'web3';
 
-export function getMetaMaskProvider() {}
+export async function getMetaMaskProvider() {
+  if (!window.ethereum) throw new Error(`No MetaMask found!`);
+
+  const web3 = new Web3(window.ethereum);
+
+  const accounts = await web3.eth.requestAccounts();
+  console.log(accounts);
+  if (!accounts || !accounts.length) throw new Error(`Permosson required!`);
+
+  return web3;
+}
